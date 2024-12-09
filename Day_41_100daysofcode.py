01f6abff62a4cf5379dc8b5585e5e97edb4d951d
javilobby2017 <javiersaucdo903@gmail.com>
2024-12-09 13:31:11 -0600

    commiting day 41

---
+++ Day_41_100daysofcode.py
@@ -0,0 +1,14 @@
+#myDictionary = {"name": "Ian", "health": 219, "strength": 99, "equipped": "Axe"}
+
+#for name, value in myDictionary.items():
+    #print(f"{name}: {value}")
+    #if(name=="strength"):
+        #if(value>100):
+            #print("whoa, SO STRONG!")
+        #else:
+            #print("weak sauce dude")
+
+myDictionary = {"name": "Ian", "health": 219, "strength": 99, "equipped": "Axe"}
+for name, value in myDictionary.items():
+    print(f"{name}: {value}")
+
