2f880bae4e36fc02ba726d3098a2b91d500436c3
javilobby2017 <javiersaucdo903@gmail.com>
2024-12-09 13:05:28 -0600

    commiting day 32

---
+++ Day_32_100daysofcode.py
@@ -0,0 +1,10 @@
+timetable = ["Computer Science" , "guitar lessons" , "ping pong class", "Art" , "sport ball"]
+
+for lesson in timetable:
+    print(lesson)
+timetable[4] = "Watch TV"
+print(timetable[0])
+print(timetable[1])
+print(timetable[2])
+print(timetable[3])
+print(timetable[4])
