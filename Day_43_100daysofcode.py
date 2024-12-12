5864adc22c6f4107a36520e2adf6f576a8e0cbc6
javilobby2017 <javiersaucdo903@gmail.com>
2024-12-12 13:43:21 -0600

    commiting Day_43_100daysofcode

---
+++ Day_43_100daysofcode.py
@@ -0,0 +1,9 @@
+#tables are two deminsional data structers that can be made horizontaly or vertically
+
+my2Dlist = [ ["johnny",21,"Mac" ],
+            ["sian", 19, "PC"],
+            ["Gethin", 17, "PC"]]
+
+(my2Dlist[1][2]) = "Linux"
+
+print(my2Dlist[1])
