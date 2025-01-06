8f6fa68f8db1e31df58773d3c8b37c9c5f87aeb5
javilobby2017 <javiersaucdo903@gmail.com>
2025-01-06 13:52:37 -0600

    day 55 altering files with imprort os

---
+++ Day_55_100daysofcode.py
@@ -0,0 +1,15 @@
+import os
+
+#print(os.listdir()) #shows files in my directory
+
+#files = os.listdir()
+#if "quickSave.txt" not in files:
+    #print("Error: Quick Save not found")
+
+# making dir with os
+#os.mkdir("hello")
+
+#re naming files 
+os.rename("myname.txt", "NEW.o")
+
+# challenge for day 55 use the challegne for day 51
